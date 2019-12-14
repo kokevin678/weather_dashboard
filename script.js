698e3deeaff0bbf93289540d89de8cefcfc0e7da
Chengyu Ko <kokevin678@gmail.com>
2019-12-13 22:03:50 -0800

    add bootstrap.css, style sidebar and main container

--- script.js
+++ script.js
@@ -2,8 +2,7 @@ $(document).ready(function() {
 
     var cityName = "San Francisco";
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=bf9f368bb7a7f00e6fbf08e7fb4f74a7";
-    
-    // console.log(queryURL);
+    console.log(queryURL);
 
     $.ajax({
         url: queryURL,
@@ -15,6 +14,22 @@ $(document).ready(function() {
         var hum = data.main.humidity;
         var wind = data.wind.speed;
         console.log(temp, hum, wind);
+
+        var currentCity = $("<h2>").text(data.name);
+        var tempEl = $("<p>").text("Temperature: " + temp + " °F");
+        var humEl = $("<p>").text("Humidity: " + hum + " %");
+        var windEl = $("<p>").text("Wind Speed: " + wind + " MPH");
+        var UV = "";
+
+        var currentContainer = $("<div class='current'>");
+        currentContainer.append(currentCity, tempEl, humEl, windEl);
+        $("#today").append(currentContainer);
+
+
+        function current(data) {
+            $()
+        }
+
     });
 
 });
