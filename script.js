a7e60a8c64573d7d1a4bc333b956c7ccaf601e2c
Chengyu Ko <kokevin678@gmail.com>
2019-12-13 22:12:58 -0800

    add moment.js, sync to current date

--- script.js
+++ script.js
@@ -10,12 +10,13 @@ $(document).ready(function() {
     }).then(function(data) {
         console.log(data);
 
+        var date = moment().format(' (MM/DD/YYYY)')
         var temp = data.main.temp;
         var hum = data.main.humidity;
         var wind = data.wind.speed;
         console.log(temp, hum, wind);
 
-        var currentCity = $("<h2>").text(data.name);
+        var currentCity = $("<h2>").text(data.name + date);
         var tempEl = $("<p>").text("Temperature: " + temp + " °F");
         var humEl = $("<p>").text("Humidity: " + hum + " %");
         var windEl = $("<p>").text("Wind Speed: " + wind + " MPH");
