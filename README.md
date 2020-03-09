4d370d5bc6e234dbfa274744c642a2a1b82e31d8
Chengyu Ko <kokevin678@gmail.com>
2020-03-08 17:37:15 -0700

    redo index.html

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# weather_dashboard
+# Weather Dashboard
+
+Weather Dashboard, an app that allows user to enter a city and display current weather.
+
+## Built With
+
+- [HTML](https://developer.mozilla.org/en-US/docs/Web/HTML)
+- [CSS](https://developer.mozilla.org/en-US/docs/Web/CSS)
+- [Javascript](https://developer.mozilla.org/en-US/docs/Web/JavaScript)
+- [Moment.js](https://momentjs.com/)
+
+## Installation Guide
+
+## Demo
+
+![Site]()
+
+## Code Snippets
+
+## Deployed Link
+
+- [See Live Site]()
+
+## Authors
+
+**Kevin Ko**
+
+- [LinkedIn](https://www.linkedin.com/in/kevin-ko-ab7a98196/)
+- [Github](https://github.com/kokevin678)
+- [Portfolio](https://kokevin678.github.io/responsive-portfolio/)
--- script.js
+++ script.js
@@ -1,59 +1,70 @@
 $(document).ready(function() {
+  var cityName = $("city").val();
+  //   var queryURL =
+  //     "https://api.openweathermap.org/data/2.5/weather?q=" +
+  //     cityName +
+  //     "&units=imperial&appid=bf9f368bb7a7f00e6fbf08e7fb4f74a7";
+  console.log(queryURL);
+  search(cityName);
 
-    var cityName = "San Francisco";
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=bf9f368bb7a7f00e6fbf08e7fb4f74a7";
-    console.log(queryURL);
-
+  function search(cityName) {
     $.ajax({
-        url: queryURL,
-        method: "GET"
+      url:
+        "https://api.openweathermap.org/data/2.5/weather?q=" +
+        cityName +
+        "&units=imperial&appid=bf9f368bb7a7f00e6fbf08e7fb4f74a7",
+      method: "GET"
     }).then(function(data) {
-        console.log(data);
+      console.log(data);
 
-        var date = moment().format(' (MM/DD/YYYY)')
-        var temp = data.main.temp;
-        var hum = data.main.humidity;
-        var wind = data.wind.speed;
-        console.log(temp, hum, wind);
+      var date = moment().format(" (MM/DD/YYYY)");
+      var temp = data.main.temp;
+      var hum = data.main.humidity;
+      var wind = data.wind.speed;
+      console.log(temp, hum, wind);
 
-        var currentCity = $("<h2>").text(data.name + date);
-        var tempEl = $("<p>").text("Temperature: " + temp + " °F");
-        var humEl = $("<p>").text("Humidity: " + hum + " %");
-        var windEl = $("<p>").text("Wind Speed: " + wind + " MPH");
-        var UV = "";
+      var currentCity = $("<h2>").text(data.name + date);
+      var tempEl = $("<p>").text("Temperature: " + temp + " °F");
+      var humEl = $("<p>").text("Humidity: " + hum + " %");
+      var windEl = $("<p>").text("Wind Speed: " + wind + " MPH");
+      var UV = "";
 
-        var currentContainer = $("<div class='current'>");
-        currentContainer.append(currentCity, tempEl, humEl, windEl);
-        $("#today").append(currentContainer);
-
+      var currentContainer = $("<div class='current'>");
+      currentContainer.append(currentCity, tempEl, humEl, windEl);
+      $("#today").append(currentContainer);
     });
 
-    var queryURLforecast = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&units=imperial&appid=bf9f368bb7a7f00e6fbf08e7fb4f74a7";
+    var queryURLforecast =
+      "https://api.openweathermap.org/data/2.5/forecast?q=" +
+      cityName +
+      "&units=imperial&appid=bf9f368bb7a7f00e6fbf08e7fb4f74a7";
     console.log(queryURLforecast);
+  }
+  $.ajax({
+    url: queryURLforecast,
+    method: "GET"
+  }).then(function(data) {
+    console.log(data.list[3]);
 
-    $.ajax({
-        url: queryURLforecast,
-        method: "GET"
-    }).then(function(data) {
-        console.log(data.list[3]);
+    var forecast = data.list;
+    for (var i = 0; i < 5; i++) {
+      var forecastDate = data.list[i].dt_txt;
+      var forecastTemp = data.list[i].main.temp;
+      var forecastHum = data.list[i].main.humidity;
+      console.log(forecastDate, forecastTemp, forecastHum);
+      // }
 
-        var forecast = data.list;
-        for (var i = 0; i < 5; i++) {
-            var forecastDate = data.list[i].dt_txt;
-            var forecastTemp = data.list[i].main.temp;
-            var forecastHum = data.list[i].main.humidity;
-            console.log(forecastDate, forecastTemp, forecastHum);
-        // }
+      var forecastDateEl = $("<p class='card-title'>").text(forecastDate);
+      var forecastTempEl = $("<p class='card text'>").text(
+        "Temp: " + forecastTemp + " °F"
+      );
+      var forecastHumEl = $("<p class='card text'>").text(
+        "Humidity: " + forecastHum + " %"
+      );
 
-        var forecastDateEl = $("<p class='card-title'>").text(forecastDate);
-        var forecastTempEl = $("<p class='card text'>").text("Temp: " + forecastTemp + " °F");
-        var forecastHumEl = $("<p class='card text'>").text("Humidity: " + forecastHum + " %");
-
-        var forecastContainer = $("<div class='card'>");
-        forecastContainer.append(forecastDateEl, forecastTempEl, forecastHumEl);
-        $("#fiveDayForecast").append(forecastContainer);
-        }
-    });
-
+      var forecastContainer = $("<div class='card'>");
+      forecastContainer.append(forecastDateEl, forecastTempEl, forecastHumEl);
+      $("#fiveDayForecast").append(forecastContainer);
+    }
+  });
 });
-
